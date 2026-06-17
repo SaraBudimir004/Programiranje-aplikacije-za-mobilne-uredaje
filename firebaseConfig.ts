b120b72e390bf1ff1af3f2f424d9f5af7640235f
Katarina <katarina.galic@fpmoz.sum.ba>
2026-06-17 14:14:13 +0200

    Dodane env konfiguracije

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAEqwYJFMneNdSB7PWm18WfMW3i2Is1bSo",
-    authDomain: "smartexpensetracker-a1d75.firebaseapp.com",
-    projectId: "smartexpensetracker-a1d75",
-    storageBucket: "smartexpensetracker-a1d75.firebasestorage.app",
-    messagingSenderId: "449100790474",
-    appId: "1:449100790474:web:c787655edbd5ed5fe55233"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
--- supabase.ts
+++ supabase.ts
@@ -1,9 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
-const supabaseUrl = "https://kmmefhhhgstbvqzsakcw.supabase.co";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supabaseAnonKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttbWVmaGhoZ3N0YnZxenNha2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MzYyOTIsImV4cCI6MjA5NTAxMjI5Mn0.TSgU2K0FtcgAHaYT1EOoCTANw0WqQZ8Hy0YJ6RKu7OM";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+    supabaseUrl,
+    supabaseAnonKey
+);
